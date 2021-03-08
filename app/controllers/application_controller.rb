e43562e4460d90d081b3cc932673a6b2e19726ef
danny galimi <dannygalimi13@aim.com>
2021-03-08 23:47:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,7 @@ class ApplicationController < Sinatra::Base
    patch "/articles/:id" do
      @article = Article.find(params[:id])
      @article.update(params[:article])
+     
      binding.pry
   end
  
